e5f714ff9b25152c42f4d20ef4d11e7715eb75a9
LeganSar44 <hoangslevan@gmail.com>
2026-01-30 10:47:25 +0700

    Update user table schema to include diabetes and CGM integration fields

--- scripts/init-db.sql
+++ scripts/init-db.sql
@@ -15,6 +15,18 @@ CREATE TABLE user(
 
 
     -- Thong tin y te
-    diabetes_type
+    diabetes_type VARCHAR(20), -- 'TYPE1',' TYPE2'
+    insulin_type VARCHAR(50), --'rapid', 'long-acting', 'mixed'
+    insulin_ratio DECIMAL(5,2), -- Units per 10g Carb
+    target_glucose_min INT DEFAULT 80,
+    target_glucose_max INT DEFAULT 140,
 
-)
+    -- CGM Integration
+    cgm_provider VARCHAR(50), --'freestyle_libre', 'dexcom', null
+    cgm_access_token TEXT,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+);
+
